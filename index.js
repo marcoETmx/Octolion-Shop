4dbe3a2a7d8fc9fabe03ae736bf60697ef9c0148
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-04-25 09:12:02 -0500

    se termina de servir archivos estaticos

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 const path = require('path')
 const productsRouter = require('./routes/products')
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use('/static', express.static(path.join(__dirname, "public")))
 
-app.set("views", path.join(__dirname,"views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug")
 
 app.use('/products', productsRouter)
 
-const server = app.listen(8000, function(){
+const server = app.listen(8000, function () {
     console.log(`Listening http://localhost:${server.address().port}`);
 })
