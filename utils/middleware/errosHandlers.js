9d7c5c1ba9a47e9115a98d5c54ad1761a12cae97
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-04-25 13:42:41 -0500

    add Sentry

--- utils/middleware/errosHandlers.js
+++ utils/middleware/errosHandlers.js
@@ -1,35 +1,41 @@
 const { config } = require("../../config");
+const Sentry = require("@sentry/node");
+
+Sentry.init({ dsn: `https://${config.sentryDns}@${config.sentryId}` });
+
 
 function logErrors(err, req, res, next) {
-  console.log(err.stack);
-  next(err);
+    Sentry.captureException(err);
+
+    console.log(err.stack);
+    next(err);
 }
 
 function clientErrorHandler(err, req, res, next) {
-  // catch errors for AJAX request
-  if (req.xhr) {
-    res.status(500).json({ err: err.message });
-  } else {
-    next(err);
-  }
+    // catch errors for AJAX request
+    if (req.xhr) {
+        res.status(500).json({ err: err.message });
+    } else {
+        next(err);
+    }
 }
 
 function errorHandler(err, req, res, next) {
-  // catch erros while streaming
-  if (res.headersSent) {
-    next(err);
-  }
+    // catch erros while streaming
+    if (res.headersSent) {
+        next(err);
+    }
 
-  if (!config.dev) {
-    delete err.stack;
-  }
+    if (!config.dev) {
+        delete err.stack;
+    }
 
-  res.status(err.status || 500);
-  res.render("error", { error: err });
+    res.status(err.status || 500);
+    res.render("error", { error: err });
 }
 
 module.exports = {
-  logErrors,
-  clientErrorHandler,
-  errorHandler
+    logErrors,
+    clientErrorHandler,
+    errorHandler
 };
